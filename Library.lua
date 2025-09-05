be4fa247293b5110a7058e30f04e8ad7f446f9cc
DaziBlaze <ltbrosens@gmail.com>
2025-09-05 18:32:43 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -49,7 +49,7 @@ local Library = {
 
     Notifications = {},
 
-    ToggleKeybind = Enum.KeyCode.RightShift,
+    ToggleKeybind = Enum.KeyCode.RightControl,
     TweenInfo = TweenInfo.new(0.1, Enum.EasingStyle.Quad, Enum.EasingDirection.Out),
     NotifyTweenInfo = TweenInfo.new(0.25, Enum.EasingStyle.Quad, Enum.EasingDirection.Out),
 
